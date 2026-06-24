edc0d9ddbfff415b8233208b32c0787c0a19aeec
Suthichoke Kultrailak <armmani@armmanis-MacBook-Air.local>
2026-06-24 15:04:22 +0700

    Double logo size on Home, Landing, Loading (keep Navbar)

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ export default function LoadingScreen() {
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white dark:bg-gray-950 z-50">
       <div className="animate-bounce">
-        <Image src="/FindTheVet.png" alt="FindTheVet" width={200} height={70} className="h-16 w-auto" priority />
+        <Image src="/FindTheVet.png" alt="FindTheVet" width={200} height={70} className="h-32 w-auto" priority />
       </div>
       <div className="flex gap-1.5 mt-6">
         <span className="w-2 h-2 rounded-full bg-primary-400 animate-bounce [animation-delay:0ms]" />
